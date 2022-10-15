72d71bd54f3b44e817cc2d559657a720f5bbf559
TranTrongTien <hoangky753@gmail.com>
2022-10-15 17:29:46 +0700

    format code

--- src/script.js
+++ src/script.js
@@ -1,17 +1,30 @@
+// Chọn tất cả img
 const imgPostion = document.querySelectorAll(".Slider_img img")
+
+// Chọn cái container của ác img
 const imgContainer = document.querySelector('.Slider_img')
 
+//
 let index = 0
-let imgNumber = imgPostion.length
-imgPostion.forEach(function(image,index){
-image.style.left = index*100 + "%"
+let imgNumber = imgPostion.length // số lượng ảnh
+console.log(imgNumber)
+
+// Để áp css left: 
+imgPostion.forEach(function(image, index) {
+    image.style.left = index * 100 + "%"
 })
+
 function imgSlide(){
-index++;
-if (index>=imgNumber) {index = 0}
-    {imgContainer.style.left=   "-" +index*100+ "%"}
+    index ++;
+    if (index >= imgNumber) {
+        index = 0
+    }
+    else {
+        imgContainer.style.left =   "-" + index * 100 + "%"
+    }
 }
-setInterval(imgSlide,5000)
+
+setInterval(imgSlide, 5000)
 
 // sticky header 
 const header = document.querySelectorAll("header")
